9af26630163177403abdc444e311dd5def0a711f
Paul Balogh <javaducky@gmail.com>
2019-05-10 17:19:00 -0500

    Initial project creation from https://start.spring.io/.
    Included dependencies for Reactive Cloud Stream, Kafka, Reactive Web, and Reactive Redis.

---
+++ src/main/java/io/weesvc/springboot/weesvc/WeesvcApplication.java
@@ -0,0 +1,13 @@
+package io.weesvc.springboot.weesvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeesvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeesvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/weesvc/springboot/weesvc/WeesvcApplicationTests.java
@@ -0,0 +1,16 @@
+package io.weesvc.springboot.weesvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WeesvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
